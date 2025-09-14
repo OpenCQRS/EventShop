c57b49e8b612204a320f23d5ce761111fec2c7a6
lucabriguglia <luca.briguglia@outlook.com>
2025-09-14 18:44:40 +0100

    Enhance AddItemToCart handler with Customer aggregate validation using domain services

--- src/EventShop.Application/Ordering/Commands/AddItemToCart.cs
+++ src/EventShop.Application/Ordering/Commands/AddItemToCart.cs
@@ -1,14 +1,31 @@
+using EventShop.Domain.Customers.Aggregates;
+using EventShop.Domain.Streams;
 using OpenCqrs.Commands;
+using OpenCqrs.EventSourcing;
 using OpenCqrs.Results;
 
 namespace EventShop.Application.Ordering.Commands;
 
 public record AddItemToCart(Guid CustomerId, Guid ShoppingCartId, Guid ProductId, int Quantity, decimal Price) : ICommand<Guid>;
 
-public class AddItemToCartHandler : ICommandHandler<AddItemToCart, Guid>
+public class AddItemToCartHandler(IDomainService domainService) : ICommandHandler<AddItemToCart, Guid>
 {
-    public Task<Result<Guid>> Handle(AddItemToCart command, CancellationToken cancellationToken = default)
+    public async Task<Result<Guid>> Handle(AddItemToCart command, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var customerStreamId = new CustomerStreamId(command.CustomerId);
+        var customerAggregateId = new CustomerAggregateId(command.CustomerId);
+        
+        var customerResult = await domainService.GetAggregate(customerStreamId, customerAggregateId, cancellationToken: cancellationToken);
+        if (customerResult.IsNotSuccess)
+        {
+            return customerResult.Failure!;
+        }
+        var customer = customerResult.Value;
+        if (customer == null)
+        {
+            return new Failure(ErrorCode.NotFound, Title: "Customer not found", Description: $"Customer with ID {command.CustomerId} not found.");
+        }
+
+        return Guid.NewGuid();
     }
 }
